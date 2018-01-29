f88f362f49d5e53cb976127f69699fd5c844bafd
yanghangfeng <yhf5fhy@gmail.com>
2018-01-29 17:59:22 +0800

    update

--- readme_generator.py
+++ readme_generator.py
@@ -7,7 +7,7 @@ import requests as rq
 class Config:
     leetcode_url = 'https://leetcode.com/problems/'
     local_path = './LeetcodeAlgorithms/'
-    github_url = 'https://github.com/Fenghuapiao/PyLeetcode'
+    github_url = 'https://github.com/Fenghuapiao/PyLeetcode/blob/master'
 
 class Question:
     """
@@ -166,7 +166,7 @@ class TableInform:
                     complete_info.solved['python'] += 1
                     # count += 1
                     folder_url = j
-                    self.table_item[i[21:24]].python = '[Python]({})'.format(Config.github_url+i[1:])
+                    self.table_item[i[21:24]].python = '[Python]({})'.format(Config.github_url+j[1:])
         readme = Readme(len(self.table),
                         complete_info.solved,
                         self.locked,
